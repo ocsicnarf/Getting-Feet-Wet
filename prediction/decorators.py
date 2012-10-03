85193ac0dff391832f900678183fd7bd93fd9b63
ocsicnarf <f.14159@gmail.com>
2012-10-03 15:51:01 -0700

    renamed a file?

--- prediction/decorators.py
+++ prediction/decorators.py
@@ -1,7 +1,25 @@
-'''Various decorator classes.'''
-import time
+"""
+utilities.py
+============
+Various decorator classes and misc. utility functions.
 
-class Cache:
+"""
+import time
+import cPickle as pickle
+
+class timed:
+    def __init__(self, function):
+        self.function = function
+        self.__name__ = function.__name__
+    
+    def __call__(self, *args):
+        start = time.time()
+        result = self.function(*args)
+        elapsed = time.time() - start
+        print '{0} took {1}s'.format(self.function.__name__, round(elapsed, 4))
+        return result
+
+class cached:
     def __init__(self, function):
         self.function = function
         self.__name__ = function.__name__
@@ -14,14 +32,52 @@ class Cache:
             self.cache[args] = self.function(*args)
             return self.cache[args]
 
-class Time:
+class pickled:
+    def __init__(self, pickle_path):
+        self.pickle_path = pickle_path
+
+    def __call__(self, function):
+        def wrapper(*args):
+            # try unpickling
+            try: 
+                file = open(self.pickle_path)
+                result = pickle.load(file)
+            except IOError:
+                pass # fail gracefully
+            else:
+                file.close()
+                return result
+            
+            # otherwise load from file, and try pickling
+            result = function(*args)
+            try: 
+                file = open(pickle_path)
+                pickle.dump(result, file)
+            except IOError:
+                print 'Error writing to pickle'
+            else:
+                file.close()
+            return result
+        return wrapper
+
+class safe:
     def __init__(self, function):
         self.function = function
         self.__name__ = function.__name__
-    
+
     def __call__(self, *args):
-        start = time.time()
-        result = self.function(*args)
-        elapsed = time.time() - start
-        print '{0} took {1:3f}s'.format(self.function.__name__, elapsed)
-        return result
+        try:
+            return function(*args)
+        except:
+            return None
+
+@safe
+def safe_float(x):
+    return float(x)
+            
+    
+
+    
+                
+
+        
